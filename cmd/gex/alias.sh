b6b7ee20a9e169d3f6d043978492abcc610d1ffc
Sebastian Stauch <sebastian.stauch@sap.com>
2018-05-22 15:49:36 +0200

    set correct path for mac

--- cmd/gex/alias.sh
+++ cmd/gex/alias.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 
-setup() 
+setup()
 {
+  os=`uname`
+  if [[ "$os" == 'Darwin' ]]; then
+    if brew ls --versions coreutils > /dev/null; then
+      export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+    else
+      echo 'run: "brew install coreutils" first'
+      exit 1
+    fi
+  fi
   local dir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
   echo $dir
   alias gex="gex_alias \"$dir\""
