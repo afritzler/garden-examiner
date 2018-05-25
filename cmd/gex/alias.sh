01c5c75adefc9af626003a4f363fca24f5c7bc0b
Uwe Krueger <uwe.krueger@sap.com>
2018-05-25 12:25:13 +0200

    fix exit status for gex alias

--- cmd/gex/alias.sh
+++ cmd/gex/alias.sh
@@ -20,11 +20,14 @@ setup()
 gex_alias()
 {
   exec 4>&1
-  local script="$("$1/gex" "${@:2}" 3>&1 >&4 || { exec 4>&-; exit 1; })"
+  local script
+  script="$("$1/gex" "${@:2}" 3>&1 >&4 || { exec 4>&-; exit 1; })"
+  local exit=$?
   exec 4>&-
   if [ -n "$script" ]; then
     source <(echo "$script")
   fi
+  return $exit
 }
 
 setup
