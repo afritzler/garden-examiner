548f02b94eb19406580fbdea337a4d200fb369e5
Uwe Krueger <uwe.krueger@sap.com>
2018-06-08 15:50:50 +0200

    check registration

--- cmd/gex/garden/register.go
+++ cmd/gex/garden/register.go
@@ -16,21 +16,22 @@ import (
 func init() {
 	filters.AddOptions(cmdline.AddAsVerb(GetCmdTab(), "register", register).Raw().
 		CmdDescription("run register for garden cluster").
-		CmdArgDescription("[email]").
-		ArgOption("garden"))
+		CmdArgDescription("[check] [<email>]")).
+		FlagOption("check").Short('c').Description("check registration")
 }
 
 func register(opts *cmdint.Options) error {
 	ctx := context.Get(opts)
+	check := opts.IsFlag("check")
 	githubURL := ""
 	if ctx.GardenSetConfig != nil {
 		githubURL = ctx.GardenSetConfig.GetGithubURL()
 	}
 	switch len(opts.Arguments) {
 	case 0:
-		return register_garden(githubURL, ctx.Garden, "")
+		return register_garden(check, githubURL, ctx.Garden, "")
 	case 1:
-		return register_garden(githubURL, ctx.Garden, opts.Arguments[0])
+		return register_garden(check, githubURL, ctx.Garden, opts.Arguments[0])
 	default:
 		return fmt.Errorf("One optional email argument required")
 	}
@@ -45,7 +46,7 @@ func getEmail(githubURL string) string {
 	return res
 }
 
-func register_garden(githubURL string, g gube.Garden, email string) error {
+func register_garden(check bool, githubURL string, g gube.Garden, email string) error {
 	if email == "" {
 		email = getEmail(githubURL)
 		if email == "null" {
@@ -62,8 +63,15 @@ func register_garden(githubURL string, g gube.Garden, email string) error {
 	}
 	for _, subject := range clusterRoleBinding.Subjects {
 		if subject.Kind == "User" && subject.Name == email {
+			if check {
+				fmt.Printf("user '%s' already registered\n", email)
+			}
 			return nil
 		}
+	}
+	if check {
+		fmt.Printf("user '%s' not registed\n", email)
+		return nil
 	}
 	clusterRoleBinding.Subjects = append(clusterRoleBinding.Subjects, rbacv1.Subject{Kind: "User", Name: email})
 	_, err = kubeset.RbacV1().ClusterRoleBindings().Update(clusterRoleBinding)
